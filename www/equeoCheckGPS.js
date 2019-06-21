bf2051332eb2e4d61c0a59ab2ea191cd9558d0d4
Sebastian Graf <sebastian.graf@equeo.de>
2019-06-21 15:01:56 +0200

    removed old files, changed call of function to check (from CheckGPS)

--- www/equeoCheckGPS.js
+++ www/equeoCheckGPS.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec')
 
-exports.CheckGPS = function (success, error, arg0) {
+exports.check = function (success, error, arg0) {
   exec(success, error, 'equeoCheckGPS', 'check', [arg0])
 }
